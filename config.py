04bc4f5143419e10a77247a19c2507cf6a495e2f
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-10-13 10:30:25 +0700

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ ASSISTANT_PREFIX = list(getenv("ASSISTANT_PREFIX", ".").split())
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "10"))
 
 ## Get it from @Botfather in Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN", "1920905087:AAE-3G2wZsjpuuSnWzQT8z_jbzwvQFAQ154")
+BOT_TOKEN = getenv("BOT_TOKEN", "1920905087:AAG_xCvsdjxVu8VUDt9s4JhD22ND-UIJttQ")
 
 ## MONGO DB
 # HOW TO GEN :-  https://telegra.ph/How-To-get-Mongodb-URI-04-06
